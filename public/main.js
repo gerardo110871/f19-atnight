7b882961d6b46920491ad387973b76196104575d
Gerardo <gpadil@icloud.com>
2022-03-17 20:28:27 -0600

    lol

--- public/main.js
+++ public/main.js
@@ -2,15 +2,15 @@ const input = document.querySelector('#input-bar')
 const button = document.querySelector('#submit-btn')
 
 function create() {
-    let nameInput = document.querySelector('#input-bar')
     const thing = {
         text: input.value
     }
     // console.log(input.value)
-    axios.post("http://localhost:4005/test", thing)
+    axios.post("/test", thing)
     .then((res) => {
         alert(res.data)
-        console.log(thing.text)
+        let input = ""
+        // console.log(thing.text)
     })
     .catch((err) => {
         console.error(err)
