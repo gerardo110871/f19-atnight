f169e37c0483c87818eee6080b0557c72e17bc1b
Gerardo <gpadil@icloud.com>
2022-03-17 21:29:55 -0600

    help

--- server/server.js
+++ server/server.js
@@ -1,44 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
+var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
-  accessToken: '276885c8eb6d4ae5980c48de4f5bffcb',
+  accessToken: '574af1c192074233a73e94c97819ebce',
   captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+  captureUnhandledRejections: true
+});
 
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+rollbar.log("Hello world!");
+//Serving files using middleware (express.static)
+// app.use(express.static(path.join(__dirname, "../public")))
 
-//serving files using middleware (express.static)
-// app.use("/", express.static(path.join(__dirname, '../public')))
+//Serving files using endpoints (app.get())
+app.get("/", function(req, res){
+    rollbar.info("We finally did it!");
+    res.sendFile(path.join(__dirname, "../public/index.html")); //Must use /index.html for endpoints.
+});
 
-//serving files using endpoints(app.get)
-app.get("/", function(req, res) {
-    rollbar.info('We finally did it')
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get("/js", (req, res)=>{
+    rollbar.info("Served JS");
+    res.sendFile(path.join(__dirname, "../public/main.js"))
 })
 
-app.get('/js', function(req, res) {
-    rollbar.info('Served JS')
-    res.sendFile(path.join(__dirname, '../public/index.js')
-})
+app.post("/yo", (req, res) =>{
+    rollbar.info(`We have received ${req.body.text}`);
+    res.sendStatus(200);
+});
 
-app.post("/test", function(req, res) {
-    rollbar.info(`we have received a response ${req.body.text}`)
-    res.sendStatus(200)
-})
+const port = process.env.PORT || 4005;
 
-const port = process.env.PORT || 4005
-
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
